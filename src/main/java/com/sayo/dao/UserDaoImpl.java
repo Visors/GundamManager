b026b1bfc82f72345f178f25e920a240b6235a2c
木之本澪 <416815311@qq.com>
2022-01-25 02:37:11 +0800

    update Dao

--- src/main/java/com/sayo/dao/UserDaoImpl.java
+++ src/main/java/com/sayo/dao/UserDaoImpl.java
@@ -4,6 +4,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import com.sayo.pojo.Good;
 import com.sayo.pojo.User;
 import com.sayo.utils.DBUtil;
 
